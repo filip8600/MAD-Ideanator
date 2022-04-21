1acfd35e08fce9ca1e7c0971a3aff28d288e8516
Filip <f-r-c@LAPTOP-BO73C4EV>
2022-04-21 13:14:56 +0200

    create og join

--- app/src/main/java/dk/au/mad22spring/appproject/group22/ideanator/lobbyActivity/LobbyActivity.java
+++ app/src/main/java/dk/au/mad22spring/appproject/group22/ideanator/lobbyActivity/LobbyActivity.java
@@ -1,16 +1,57 @@
 package dk.au.mad22spring.appproject.group22.ideanator.lobbyActivity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import dk.au.mad22spring.appproject.group22.ideanator.R;
 
 public class LobbyActivity extends AppCompatActivity {
+    private Button startButton;
+    private ActivityResultLauncher<Intent> launcher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        setupUI();
+        setupListeners();
+        setupLauncher();
     }
+
+    private void setupUI() {
+        startButton=findViewById(R.id.LobbyBtnStart);
+    }
+
+    private void setupLauncher() {
+        //Launcher - Based on code from MAD lecture 2
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+            }
+        });
+    }
+    private void setupListeners() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
+
+    }
+
+    private void startGame() {
+    }
+
 }
