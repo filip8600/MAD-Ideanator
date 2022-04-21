ad534b883a395c11b649adc41db052baec21d154
Filip <f-r-c@LAPTOP-BO73C4EV>
2022-04-21 11:07:36 +0200

    added activity folders

--- app/src/main/java/dk/au/mad22spring/appproject/group22/ideanator/mainActivity/MainActivity.java
+++ app/src/main/java/dk/au/mad22spring/appproject/group22/ideanator/mainActivity/MainActivity.java
@@ -1,8 +1,10 @@
-package dk.au.mad22spring.appproject.group22.ideanator;
+package dk.au.mad22spring.appproject.group22.ideanator.mainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import dk.au.mad22spring.appproject.group22.ideanator.R;
 
 public class MainActivity extends AppCompatActivity {
 
